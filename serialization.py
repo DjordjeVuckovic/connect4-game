a27f29fff529cc386b49d36b925410cb06d496ec
Djordje Vuckovic <93745016+DjordjeVuckovic@users.noreply.github.com>
2023-02-02 21:22:16 +0100

    final project

---
+++ serialization.py
@@ -0,0 +1,31 @@
+"""
+Modul koji vrsi citanje i pisanje iz dataSeta
+"""
+import csv
+from matplotlib import pyplot as plt
+
+def read():
+    x = []
+    y = []
+    with open(r"dataSet.csv", 'r') as csvfile:
+        read = csv.reader(csvfile, delimiter=',')
+        next(read)
+        for entitet in read:
+            x.append(int(entitet[0]))
+            y.append(int(entitet[1]))
+        return x,y
+        
+def write(turn,column):
+    with open('dataSet.csv','a',newline='') as csvfile:
+        write = csv.DictWriter(csvfile,['Turn','Column'],delimiter=',')
+        write.writerow({'Turn':turn+1,'Column': column}) 
+
+def plot_dataSet():
+    x,y=read()
+    ax1 = plt.scatter(x,y,color='blue')
+    plt.legend(labels=['Turn', 'Position'])
+    plt.title('Linear regression', size=24)
+    plt.xlabel('Turn', size=18)
+    plt.ylabel('Position', size=18);
+    plt.show()
+
